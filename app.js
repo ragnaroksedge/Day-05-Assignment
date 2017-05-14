3400b13c3fd9d2549f22e6e17e33a2ddfb3171ad
Allan Liebold <allan.liebold@gmail.com>
2017-05-14 12:25:58 -0700

    prepared app.js to start working on problem 4

--- app.js
+++ app.js
@@ -37,14 +37,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 
 function multiply(a,b){ //eslint-disable-line
-
+  var myProd = a * b;
+  var myMsg = 'The product of ' + a + ' and ' + b + ' is ' + myProd + '.';
+  return [myProd, myMsg];
 }
 
 
 
 // Here is the test for multiply(); uncomment it to run it
 
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 /////////////////////////////////////
@@ -63,10 +65,19 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 
 function sumAndMultiply(a,b,c){ //eslint-disable-line
+  var sumOfTwo = sum(a,b);
+  var sumOfThree = sum(sumOfTwo[0],c);
+  var myMsgSum = a + ' and ' + b + ' and ' + c + ' sum to ' + sumOfThree[0] + '.';
+  var prodOfTwo = multiply(a,b);
+  var prodOfThree = multiply(prodOfTwo[0],c);
+  var myMsgProd = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + prodOfThree[0] + '.';
+  return [sumOfThree[0], prodOfThree[0], myMsgSum, myMsgProd];
 }
 
+sumAndMultiply();
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
